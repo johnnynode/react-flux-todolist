c9f9a5ea89d135cb926c292745c30093ecae2391
johnnynode <johnnynode@gmail.com>
2018-03-08 21:55:40 +0800

    add flux container optimization

--- src/View/TodoHeaderContainer.js
+++ src/View/TodoHeaderContainer.js
@@ -2,23 +2,16 @@ import React, {Component} from 'react';
 import TodoHeader from "./TodoHeader";
 
 import TodoStore from '../Store/TodoStore';
+import {Container} from 'flux/utils';
 
 class TodoHeaderContainer extends Component {
+  /*
   constructor(props) {
     super(props);
     // 将数据存放在状态机中, 状态机改变, 自动刷新
     this.state = {
       todos: TodoStore.getState()
     };
-  }
-
-  render() {
-    const { todos } = this.state;
-    let todoCount = todos.filter((todo) => !todo.checked).length;
-    
-    return (
-      <TodoHeader name="汪峰" todoCount={todoCount} />
-    )
   }
 
   componentDidMount() {
@@ -32,6 +25,26 @@ class TodoHeaderContainer extends Component {
   componentWillUnmount() {
     this.addObserver();
   }
+  */
+
+  static getStores() {
+    return [TodoStore];
+  }
+
+  static calculateState(prevState) {
+    return {
+      todos: TodoStore.getState()
+    }
+  }
+
+  render() {
+    const { todos } = this.state;
+    let todoCount = todos.filter((todo) => !todo.checked).length;
+    
+    return (
+      <TodoHeader name="汪峰" todoCount={todoCount} />
+    )
+  }
 }
 
-export default TodoHeaderContainer
+export default Container.create(TodoHeaderContainer); 
--- src/View/TodoListContainer.js
+++ src/View/TodoListContainer.js
@@ -3,27 +3,16 @@ import TodoList from "./TodoList";
 
 import TodoAction from '../Action/TodoAction';
 import TodoStore from '../Store/TodoStore';
+import {Container} from 'flux/utils';
 
 class TodoListContainer extends Component {
+  /*
   constructor(props) {
     super(props);
     // 将数据存放在状态机中, 状态机改变, 自动刷新
     this.state = {
       todos: TodoStore.getState()
     };
-  }
-
-
-  render() {
-    const { todos } = this.state;
-    return (
-      <TodoList 
-          todos={todos} 
-          toggleItemList={TodoAction.toggleItem}
-          delItemList={TodoAction.delItem}
-          editItemList={TodoAction.editItem}
-        />
-    )
   }
 
   componentDidMount() {
@@ -37,6 +26,29 @@ class TodoListContainer extends Component {
   componentWillUnmount() {
     this.addObserver();
   }
+  */
+  
+  static getStores() {
+    return [TodoStore];
+  }
+
+  static calculateState(prevState) {
+    return {
+      todos: TodoStore.getState()
+    }
+  }
+
+  render() {
+    const { todos } = this.state;
+    return (
+      <TodoList 
+          todos={todos} 
+          toggleItemList={TodoAction.toggleItem}
+          delItemList={TodoAction.delItem}
+          editItemList={TodoAction.editItem}
+        />
+    )
+  }
 }
 
-export default TodoListContainer
+export default Container.create(TodoListContainer);
