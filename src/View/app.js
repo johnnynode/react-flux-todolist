abbb2cc9dd0b7bd3a27440dd7dee7f9bb781b33f
johnnynode <johnnynode@gmail.com>
2018-03-21 12:31:50 +0800

    rename container

--- src/View/app.js
+++ src/View/app.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import Action from '../Action';
-import HeaderContainer from './container/headerContainer';
-import InputContainer from './container/inputContainer';
-import ListContainer from './container/listContainer';
+import HeaderContainer from './container/header-container';
+import InputContainer from './container/input-container';
+import ListContainer from './container/list-container';
 
 class App extends Component {
   render() {
