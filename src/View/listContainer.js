0c4dcd0d66a353863e1907903a22445549988ad0
johnnynode <johnnynode@gmail.com>
2018-03-21 11:05:55 +0800

    add listContainer

---
+++ src/View/listContainer.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+
+import Action from '../Action';
+import Store from '../Store';
+import {Container} from 'flux/utils';
+
+import List from './list';
+
+class ListContainer extends Component {
+  static getStores() {
+    return [Store];
+  }
+
+  static calculateState() {
+    return {
+      data: Store.getState()
+    }
+  }
+
+  render() {
+    const {
+      data
+    } = this.state;
+
+    return (
+      <List 
+      bookList={data} 
+      toggleItemList={Action.toggleItem}
+      delItemList={Action.delItem}
+      editItemList={Action.editItem}
+      />
+    )
+  }
+}
+
+export default Container.create(ListContainer);
