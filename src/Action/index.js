d1d8979074a55dac5fd808d35e3e8089321cc408
johnnynode <johnnynode@gmail.com>
2018-03-21 09:48:34 +0800

    add Action

---
+++ src/Action/index.js
@@ -0,0 +1,42 @@
+import Constants from '../Constants';
+import Dispatcher from '../Dispatcher';
+
+const Action = {
+  toggleItem(id) {
+    Dispatcher.dispatch({
+      id,
+      type:Constants.toggleItem
+    })
+  },
+  delItem(id) {
+    Dispatcher.dispatch({
+      id,
+      type: Constants.delItem
+    })
+  },
+  createItem(title) {
+    Dispatcher.dispatch({
+      title,
+      type: Constants.createItem
+    });
+  },
+  editItem(id, title) {
+    Dispatcher.dispatch({
+      id,
+      title,
+      type:Constants.delItem
+    });
+  },
+  loadDate() {
+    fetch('todos.json')
+    .then((data)=>data.json())
+    .then((data)=>{
+      Dispatcher.dispatch({
+        data,
+        type:Constants.loadData
+      });
+    })
+  }
+}
+
+export default Action;
