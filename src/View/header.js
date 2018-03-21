15b7902642b9da7f3e6a1cfc87209bfae9d6b751
johnnynode <johnnynode@gmail.com>
2018-03-21 10:14:20 +0800

    finish view header

---
+++ src/View/header.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+  static get defaultProps(props) {
+    return {
+      name: "Johnny",
+      bookLeft:0
+    }
+  }
+
+  render() {
+    const {
+      name,
+      bookLeft
+    } = this.props;
+    return (
+      <div>
+        <h1>React's Basic Todo List</h1>
+        <div>Hello {name} , you have {bookLeft} books left to read!</div>
+      </div>
+    )
+  }
+}
+
+export default Header;
