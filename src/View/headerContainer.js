d03a51f0601a147cf3e9ac7af2d4e458a559cfe3
johnnynode <johnnynode@gmail.com>
2018-03-21 10:22:38 +0800

    finish headerContainer

---
+++ src/View/headerContainer.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+import Store from '../Store';
+import {Container} from 'flux/utils';
+
+import Header from './header';
+
+class HeaderContainer extends Component {
+  static getStores() {
+    return [Store];
+  }
+
+  static calculateState() {
+    return {
+      data: Store.getState()
+    }
+  }
+
+  render() {
+    const {
+      data
+    } = this.state;
+    let bookLeft =  data.filter((item) => !item.checked).length;
+
+    return (
+      <Header name="John" bookLeft={bookLeft} />
+    )
+  }
+}
+
+export default Container.create(HeaderContainer);
